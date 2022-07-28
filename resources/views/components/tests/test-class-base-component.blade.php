0d82db4d5118c3ffe5fab961feed98b0fb4fdd3f
sasatank <sasa.tankytank@gmail.com>
2022-07-28 23:04:08 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+    クラスベースのコンポーネントです。
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+</div>
