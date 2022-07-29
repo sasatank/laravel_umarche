68d66c1eedbd2358a96dec8687922fc71b322d0d
sasatank <sasa.tankytank@gmail.com>
2022-07-29 16:47:44 +0900

    サービスプロバイダーのテスト

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
+    public function showServiceProviderTest(){
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample = app()->make('serviceProviderTest');
+
+        dd($sample,$password,$encrypt->decrypt($password));
+    }
+
     public function showServiceContainerTest(){
         app()->bind('lifeCycleTest',function(){
             return 'ライフサイクルテスト';
@@ -21,6 +30,8 @@ class LifeCycleTestController extends Controller
         app()->bind('sample',Sample::class);
         $sample = app()->make('sample');
         $sample->run();
+
+
 
         dd($test,app());
     }
